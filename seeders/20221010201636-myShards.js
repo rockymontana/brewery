2f1edeca0f9a2aa61860a7d57fe819c080863c15
Jonas Erlandsson <rockymontana@users.noreply.github.com>
2025-07-07 22:45:45 +0200

    Update 20221010201636-myShards.js

--- seeders/20221010201636-myShards.js
+++ seeders/20221010201636-myShards.js
@@ -1,15 +1,5 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert('shards', [{
-      id: 1,
-      userName: 'jerlandsson',
-      userId: '506568581559877633',
-      ancients: 0,
-      voids: 0,
-      sacreds: 0,
-      createdAt: "2022-10-10 22:22",
-      updatedAt: "2022-10-10 22:22"
-    }]);
   },
   down: (queryInterface, Sequelize) => {
     return queryInterface.bulkDelete('Users', null, {});
